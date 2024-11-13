ef214447a96b2c05bfc19f446beee446bc9aad11
mhcal <mhcal@tutanota.com>
2024-11-13 23:20:02 +0000

    Added Foldable instance for BTrees

---
+++ BTree.hs
@@ -0,0 +1,10 @@
+-- type for binary tree:
+data BTree a = Empty
+             | Node a (BTree a) (BTree a)
+
+instance Foldable BTree where -- foldable instance for binary trees
+-- the minimal instance definitions for the Foldable class are foldMap and foldr
+  foldMap g (Node n l r) = g n <> foldMap g l <> foldMap g r
+  foldr f z Empty = z
+  foldr f z (Node n l r) = foldr f (f n (foldr f z r)) l
+  
